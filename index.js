dccf59974f6f13b4abec77e374bcc114ab74218e
Mr.Bishop <137743027+Gehanbalasooriya@users.noreply.github.com>
2023-12-27 22:24:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+ 
+  document.getElementById("enter").onclick =function() {
+
+  var username = document.getElementById("name").value;
+  var name = "kavin";
+  
+  var password =document.getElementById("pass").value;
+  var pass = "127";
+  
+
+
+  if (name==username & pass==password){
+  
+
+    alert("I am an alert box!");
+  }
+
+  else{}
+    
+  }
+
+
+
+
